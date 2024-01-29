625d19260530cefc2ce6b58763ac381043c95ef6
Isaac Lyman <github@isaaclyman.com>
2024-01-29 12:00:00 -0700

    Autoformat

--- k4-attempts/8-math-substitution.ts
+++ k4-attempts/8-math-substitution.ts
@@ -8,7 +8,9 @@ import { generateAlphabet, standardAlphabet } from '../lib/vigenere';
 
 const k4length = encryptedK4.length;
 
-const countingSet = Array(k4length).fill(null).map((_, ix) => ix + 1);
+const countingSet = Array(k4length)
+  .fill(null)
+  .map((_, ix) => ix + 1);
 tryAllKeyArrangements(encryptedK4, countingSet, 'countingSet');
 
 const naturalSet = [0, ...countingSet];
@@ -38,26 +40,37 @@ tryAllKeyArrangements(encryptedK4, berlinClock6, 'berlinClock6');
 const berlinClock7 = [1, 2, 3, 4, 5];
 tryAllKeyArrangements(encryptedK4, berlinClock7, 'berlinClock7');
 
-const powersOfFive = countingSet.map(val => 5 ** (val % 5));
+const powersOfFive = countingSet.map((val) => 5 ** (val % 5));
 tryAllKeyArrangements(encryptedK4, powersOfFive, 'powersOfFive');
 
-const powersOfFive2 = naturalSet.map(val => 5 ** (val % 5));
+const powersOfFive2 = naturalSet.map((val) => 5 ** (val % 5));
 tryAllKeyArrangements(encryptedK4, powersOfFive2, 'powersOfFive2');
 
-const powersOfFive3 = naturalSet.map(val => (val % 5) ** 5);
+const powersOfFive3 = naturalSet.map((val) => (val % 5) ** 5);
 tryAllKeyArrangements(encryptedK4, powersOfFive3, 'powersOfFive3');
 
 for (let multiple = 2; multiple <= 24; multiple++) {
-  const multiplesSet1 = countingSet.map(val => val * multiple);
-  tryAllKeyArrangements(encryptedK4, multiplesSet1, 'multiplesOf' + multiple + 'Counting')
-  const multiplesSet2 = naturalSet.map(val => val * multiple);
-  tryAllKeyArrangements(encryptedK4, multiplesSet2, 'multiplesOf' + multiple + 'Natural')
-
+  const multiplesSet1 = countingSet.map((val) => val * multiple);
+  tryAllKeyArrangements(
+    encryptedK4,
+    multiplesSet1,
+    'multiplesOf' + multiple + 'Counting'
+  );
+  const multiplesSet2 = naturalSet.map((val) => val * multiple);
+  tryAllKeyArrangements(
+    encryptedK4,
+    multiplesSet2,
+    'multiplesOf' + multiple + 'Natural'
+  );
 }
 
-function tryAllKeyArrangements(message: string, key: number[], description: string) {
+function tryAllKeyArrangements(
+  message: string,
+  key: number[],
+  description: string
+) {
   for (let increment = 0; increment < key.length; increment++) {
-    const newKey = key.map((_, ix) => key[((ix + increment) % key.length)]);
+    const newKey = key.map((_, ix) => key[(ix + increment) % key.length]);
     messageForwardAndReverse(message, newKey, description);
   }
 }
