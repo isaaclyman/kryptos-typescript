f8b3848ee589c2566fbf76734d5ca36fbfa7d977
Isaac Lyman <github@isaaclyman.com>
2024-01-30 11:22:10 -0700

    Update 9-hill

--- k4-attempts/9-hill.ts
+++ k4-attempts/9-hill.ts
@@ -4,8 +4,19 @@ import { chunk } from "../lib/array";
 import { getCoincidence } from "../lib/coincidence";
 import { hillDecrypt } from "../lib/hill";
 
-const berlinKey1 = chunk(lettersToNumbers('BERLINUHR', generateAlphabet('KRYPTOS')), 3);
+const kAlpha = generateAlphabet('KRYPTOS');
+
+const berlinKey1 = chunk(lettersToNumbers('BERLINUHR', kAlpha), 3);
 const berlinKey1Result = hillDecrypt(encryptedK4 + 'XX', berlinKey1, standardAlphabet);
 console.log({berlinKey1, result: berlinKey1Result, c: getCoincidence(berlinKey1Result)});
 
-// It's actually pretty hard to make up a matrix with a non-zero determinant, apparently
+// As written:
+// ZDPLMFPQNJXUVAPTLYSHOCEXNUJYIVGESSJJRLZLVLYEQVSIIZVPFQHJVKUMGWJRFEKIJPCANYLTXRMYDXLQMSAHNDYKKEOGYCL
+// c=0.037
+
+// Reverse the key:
+// VXTDBATXWODPPNIUPXCGRFWUXTMLSIAWYBENVPVBFVYWQSVYTOXAVFNRPAGWBEOOVHZSUELYHPELDZZCQMPBKICDNHEQEIMCPYU
+// c=0.036
+
+// It's actually pretty hard to make up a matrix with a non-zero determinant, apparently.
+// I have yet to find another valid key based on any clues.
